ec1db71b417185b59365eb650dc835965b7efa65
Ketevan Zirakashvili <95405261+ketizi@users.noreply.github.com>
2023-06-28 12:24:33 +0400

    update

--- src/test/java/Contact.java
+++ src/test/java/Contact.java
@@ -42,8 +42,7 @@ public class Contact extends ChromeRunner {
         softAssertion.assertTrue(driver.getCurrentUrl().contains("contact"));
 
 
-
-        // this is commit // this is commit // this is commit // this is commit // this is commit
+        
 
 
     }
